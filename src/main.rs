73ea52d8a3657a0b68d0a6ac68f9bd15dfac1f32
retereum <pavelnikolaichev@gmail.com>
2024-10-06 22:40:19 +0800

    Task L1.9 finished

--- src/main.rs
+++ src/main.rs
@@ -1,149 +1,38 @@
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use std::thread;
-use dashmap::DashMap;
+use std::io::{self};
 
-fn mutex_with_hashmap() {
-    let map = Arc::new(Mutex::new(HashMap::new()));
-    let mut threads = vec![];
-
-    for i in 0..10 {
-        let map = Arc::clone(&map);
-        threads.push(thread::spawn(move || {
-            let mut map = map.lock().unwrap();
-            map.insert(i, i * 2);
-            println!("Inserted key: {}, value: {}", i, i * 2);
-        }));
+fn set_bit(mut num: i64, bit_position: usize, value: u8) -> i64 {
+    if bit_position > 63 {
+        panic!("Bit position must be between 0 and 63");
     }
 
-    for thread in threads {
-        thread.join().unwrap();
+    match value {
+        1 => num |= 1 << bit_position,
+        0 => num &= !(1 << bit_position),
+        _ => panic!("Value must be 0 or 1"),
     }
 
-    println!("Final map: {:?}", *map.lock().unwrap());
-
-    println!("Concurrent read from HashMap:");
-    let mut threads = vec![];
-
-    for i in 0..10 {
-        let map = Arc::clone(&map);
-        threads.push(thread::spawn(move || {
-            let map = map.lock().unwrap();
-            let value = map.get(&i);
-            match value {
-                Some(v) => println!("Key: {}, Value: {}", i, v),
-                None => println!("Key: {} not found", i),
-            }
-        }));
-    }
-
-    for thread in threads {
-        thread.join().unwrap();
-    }
-
-    println!("Concurrent read is done");
-
-    println!("Concurrent read-write from HashMap:");
-    let mut threads = vec![];
-
-    for i in 0..10 {
-        let map = Arc::clone(&map);
-        if i % 2 == 0 {
-            threads.push(thread::spawn(move || {
-                let map = map.lock().unwrap();
-                let value = map.get(&i);
-                match value {
-                    Some(v) => println!("Key: {}, Value: {}", i, v),
-                    None => println!("Key: {} not found", i),
-                }
-            }));
-        } else {
-            threads.push(thread::spawn(move || {
-                let mut map = map.lock().unwrap();
-                map.insert(i, i * 3);
-                println!("Inserted key: {}, value: {}", i, i * 3);
-            }));
-        }
-    }
-
-    for thread in threads {
-        thread.join().unwrap();
-    }
-
-    println!("Concurrent read-write is done");
-}
-
-fn dashmap_concurrent_write() {
-    let map = Arc::new(DashMap::new());
-    let mut threads = vec![];
-
-    for i in 0..10 {
-        let map = Arc::clone(&map);
-        threads.push(thread::spawn(move || {
-            map.insert(i, i * 2);
-            println!("Inserted key: {}, value: {}", i, i * 2);
-        }));
-    }
-
-    for thread in threads {
-        thread.join().unwrap();
-    }
-
-    println!("Final map: {:?}", map);
-
-    println!("Concurrent read from DashMap:");
-    let mut threads = vec![];
-
-    for i in 0..10 {
-        let map = Arc::clone(&map);
-        threads.push(thread::spawn(move || {
-            let value = map.get(&i);
-            match value {
-                Some(v) => println!("Key: {}, Value: {}", i, v.value()),
-                None => println!("Key: {} not found", i),
-            }
-        }));
-    }
-
-    for thread in threads {
-        thread.join().unwrap();
-    }
-
-    println!("Concurrent read is done");
-
-    println!("Concurrent read-write from DashMap:");
-
-    let mut threads = vec![];
-
-    for i in 0..10 {
-        let map = Arc::clone(&map);
-        if i % 2 == 0 {
-            threads.push(thread::spawn(move || {
-                let value = map.get(&i);
-                match value {
-                    Some(v) => println!("Key: {}, Value: {}", i, v.value()),
-                    None => println!("Key: {} not found", i),
-                }
-            }));
-        } else {
-            threads.push(thread::spawn(move || {
-                map.insert(i, i * 3);
-                println!("Inserted key: {}, value: {}", i, i * 3);
-            }));
-        }
-    }
-
-    for thread in threads {
-        thread.join().unwrap();
-    }
-
-    println!("Concurrent read-write is done");
+    num
 }
 
 fn main() {
-    println!("=== Тестирование Mutex с HashMap ===");
-    mutex_with_hashmap();
+    println!("Введите число: ");
+    let mut input = String::new();
 
-    println!("\n=== Тестирование DashMap ===");
-    dashmap_concurrent_write();
+    io::stdin().read_line(&mut input).unwrap();
+    let num: i64 = input.trim().parse().unwrap();
+
+    println!("Введите позицию бита (0-63): ");
+    input.clear();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let bit_position: usize = input.trim().parse().unwrap();
+
+    println!("Введите значение бита (0 или 1): ");
+    input.clear();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let value: u8 = input.trim().parse().unwrap();
+
+    let result = set_bit(num, bit_position, value);
+    println!("Результат: {}", result);
 }
