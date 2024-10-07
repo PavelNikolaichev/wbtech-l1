53b7a946e04f6720857fa7863207ffb918dd9e5e
retereum <pavelnikolaichev@gmail.com>
2024-10-07 08:59:38 +0800

    Task L1.14 finished

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
+use std::any::Any;
+
+fn get_type<T: Any>(_: T) {
+    println!("{}", std::any::type_name::<T>());
+}
+
 fn main() {
-    let mut strings: Vec<String> = Vec::new();
-
-    println!("Введите кол-во строк:");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-
-    let n: i32 = input.trim().parse().unwrap();
-
-    for i in 0..n {
-        input.clear();
-        println!("Введите строку {}:", i+1);
-        std::io::stdin().read_line(&mut input).unwrap();
-
-        // O(n) поиск, с хеш-таблицей было бы O(1) :(
-        if !strings.contains(&input.trim().to_string()) {
-            strings.push(input.trim().to_string());
-        }
-    }
-
-    println!("Уникальные строки: \n{}", strings.join("\n"));
+    get_type(42);
+    get_type("Hello");
+    get_type(3.14);
+    get_type(true);
+    get_type('A');
+    get_type([1, 2, 3]);
+    get_type((1, 2, 3));
+    get_type(Some(42));
+    get_type(Box::new(42));
+    get_type(String::from("Hello"));
+    get_type(vec![1, 2, 3]);
+    get_type(std::collections::HashMap::<i32, i32>::new());
+    get_type(std::collections::HashSet::<i32>::new());
 }
