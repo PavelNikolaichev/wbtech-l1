797faaa71248061e4a82c61fe2ce799008e7a96c
retereum <pavelnikolaichev@gmail.com>
2024-10-07 10:44:02 +0800

    Task L1.19 finished

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,10 @@ fn main() {
     std::io::stdin().read_line(&mut input).unwrap();
     let input = input.trim().to_string();
 
-    if input.is_empty() {
-        println!("Результат: ");
-        return;
-    }
+    let result = input.rsplit(' ')
+        .map(|word| word.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
 
-    let mut l = 0;
-    let mut r = input.len() - 1;
-
-    let mut chars: Vec<char> = input.chars().collect();
-
-    while l < r {
-        chars.swap(l, r);
-
-        l += 1;
-        r -= 1;
-    }
-
-    println!("Результат: {}", chars.into_iter().collect::<String>());
+    println!("Результат: {}", result);
 }
