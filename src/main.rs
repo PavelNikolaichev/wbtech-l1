df707261b3466e0e2eb400cbf6e8f05cb119a6d7
retereum <pavelnikolaichev@gmail.com>
2024-10-07 10:05:55 +0800

    Task L1.16 finished

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,46 @@
 use rand::random;
 
-fn quicksort(arr: &mut [i32]) {
-    if arr.len() <= 1 {
-        return;
-    }
+fn binary_search(arr: &Vec<i32>, target: i32) -> i32 {
+    let mut left = 0;
+    let mut right = arr.len() as i32 - 1;
 
-    let pivot = partition(arr);
-
-    quicksort(&mut arr[0..pivot]);
-    quicksort(&mut arr[pivot + 1..]);
-}
-
-fn partition(arr: &mut [i32]) -> usize {
-    let pivot = arr[arr.len() - 1];
-    let mut index = 0;
-
-    for i in 0..arr.len() - 1 {
-        if arr[i] < pivot {
-            arr.swap(index, i);
-            index += 1;
+    while left <= right {
+        let mid = left + (right - left) / 2;
+        if arr[mid as usize] == target {
+            return mid;
+        } else if arr[mid as usize] < target {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-    arr.swap(index, arr.len() - 1);
 
-    index
+    return -1;
 }
 
 fn main() {
     let mut array: Vec<i32> = (0..10).map(|_| i32::abs(random::<i32>() % 100)).collect();
-    let mut sorted_array = array.clone();
-
-    println!("Сгенерированный массив: {:?}", array);
-
-    quicksort(&mut sorted_array);
     array.sort();
 
-    println!("Массив, отсортированный quick sort: {:?}", sorted_array);
-    println!("Массив, отсортированный  `.sort()`: {:?}", array);
+    let target = array[i32::abs(random::<i32>() % 10) as usize];
+
+    println!("Сгенерированный массив: {:?}", array);
+    println!("Искомый элемент из массива: {}", target);
+
+    let result = binary_search(&array, target);
+    if result != -1 {
+        println!("Элемент найден под индексом: {}", result);
+    } else {
+        println!("Элемент не найден");
+    }
+
+    let target = 101; // out of range
+
+    println!("Искомый элемент из массива: {}", target);
+    let result = binary_search(&array, target);
+    if result != -1 {
+        println!("Элемент найден под индексом: {}", result);
+    } else {
+        println!("Элемент не найден");
+    }
 }
