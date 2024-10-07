58b9caa674b94e7c1cc416305a38feec6ab6c4e2
retereum <pavelnikolaichev@gmail.com>
2024-10-07 10:16:54 +0800

    Task L1.17 finished

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,39 @@
-use rand::random;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-fn binary_search(arr: &Vec<i32>, target: i32) -> i32 {
-    let mut left = 0;
-    let mut right = arr.len() as i32 - 1;
+#[derive(Debug)]
+struct Counter {
+    count: Mutex<i32>
+}
 
-    while left <= right {
-        let mid = left + (right - left) / 2;
-        if arr[mid as usize] == target {
-            return mid;
-        } else if arr[mid as usize] < target {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+impl Counter {
+    fn increment(&self) {
+        let mut count = self.count.lock().unwrap();
+        *count += 1;
     }
 
-    return -1;
+    fn get(&self) -> i32 {
+        let count = self.count.lock().unwrap();
+        *count
+    }
 }
 
 fn main() {
-    let mut array: Vec<i32> = (0..10).map(|_| i32::abs(random::<i32>() % 100)).collect();
-    array.sort();
+    let counter = Arc::new(Counter { count: Mutex::new(0) });
+    let mut threads = vec![];
 
-    let target = array[i32::abs(random::<i32>() % 10) as usize];
-
-    println!("Сгенерированный массив: {:?}", array);
-    println!("Искомый элемент из массива: {}", target);
-
-    let result = binary_search(&array, target);
-    if result != -1 {
-        println!("Элемент найден под индексом: {}", result);
-    } else {
-        println!("Элемент не найден");
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        threads.push(thread::spawn(move || {
+            for _ in 0..1000 {
+                counter.increment();
+            }
+        }));
     }
 
-    let target = 101; // out of range
-
-    println!("Искомый элемент из массива: {}", target);
-    let result = binary_search(&array, target);
-    if result != -1 {
-        println!("Элемент найден под индексом: {}", result);
-    } else {
-        println!("Элемент не найден");
+    for thread in threads {
+        thread.join().unwrap();
     }
+
+    println!("Результат: {}", counter.get());
 }
